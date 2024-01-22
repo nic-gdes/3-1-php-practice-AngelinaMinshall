1f22d7bbbb8797abe6cb027f59d9600d04fb20f9
Angelina Minshall <atminshall@nic.edu>
2024-01-22 15:49:08 -0800

    link head

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
+
+<?php 
+    $page_title = "Home"
+    include('./components/head.php'); 
+?>
+
+
 <body>
     <h1>Homepage</h1>
 </body>
