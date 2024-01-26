fece09f4eac4079071ad5051f7d81d339a588981
Angelina Minshall <atminshall@nic.edu>
2024-01-26 11:50:20 -0800

    fixing header

---
+++ components/grid-item.php
@@ -0,0 +1,33 @@
+<main>
+        <h1>My Favorite Pufferfish</h1>
+        <p>Pufferfish are fascinating creatures known for their unique appearance and behavior. They have the ability to inflate themselves when threatened, making them both cute and a bit quirky.</p>
+
+        <div class="grid-container">
+
+            <div class="grid-item">
+                <img src="./assests/dwarf.jpg" alt="Dwarf Pufferfish">
+                <h3>Dwarf Pufferfish</h3>
+            </div>
+            <div class="grid-item">
+                <img src="fahaka.jpg" alt="Fahaka Pufferfish">
+                <h3>Fahaka Pufferfish</h3>
+            </div>
+            <div class="grid-item">
+                <img src="blackspotted.jpg" alt="Black Spotted Pufferfish">
+                <h3>Black Spotted Pufferfish</h3>
+            </div>
+            <div class="grid-item">
+                <img src="tetraodon.jpg" alt=" Tetraodon Pufferfish">
+                <h3>Tetraodon Pufferfish</h3>
+            </div>
+            <div class="grid-item">
+                <img src="toby.jpg" alt="Black saddled toby Pufferfish"> 
+                <h3>Black saddled toby Pufferfish</h3>
+            </div>
+            <div class="grid-item">
+                <img src="northern.jpg" alt="Northern Pufferfish">
+                <h3>Northern Pufferfish</h3>
+            </div>
+      
+        </div>
+    </main>
---
+++ components/nav.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <a href="index.php">
+            <img src="pufferfish.png" alt="Pufferfish">
+        </a>
+        <a href="index.php">Home</a>
+        <a href="contact.php">Contact</a>
+    </nav>
+    <h1>Homepage</h1>
+</header>
